02ecd21c8491e26a733a2543ecc3ff1b9c0ac326
Walze <catufuzgu@gmail.com>
2017-11-30 11:22:21 -0200

    router works

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,16 +2,39 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
-import { AppComponent } from './app.component';
+import { RouterModule, Routes } from '@angular/router';
 
+import { AppComponent } from './app.component';
+import { CadUsuarioComponent } from './cad-usuario/cad-usuario.component';
+import { LoginUsuarioComponent } from './login-usuario/login-usuario.component';
+
+const appRoutes: Routes = [
+  {
+    path: 'cadastro',
+    component: CadUsuarioComponent
+  },
+  {
+    path: 'login',
+    component: LoginUsuarioComponent
+  },
+  {
+    path: '',
+    redirectTo: '/cadastro',
+    pathMatch: 'full'
+  },
+  { path: '**', component: LoginUsuarioComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CadUsuarioComponent,
+    LoginUsuarioComponent
   ],
   imports: [
     NgbModule.forRoot(),
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes, { enableTracing: true })
   ],
   providers: [],
   bootstrap: [AppComponent]
