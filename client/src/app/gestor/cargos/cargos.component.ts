9e28837d75f93419b65f911ed276fc913c3b1293
Walze <catufuzgu@gmail.com>
2018-01-05 13:38:52 -0200

    finished cargos

--- client/src/app/gestor/cargos/cargos.component.ts
+++ client/src/app/gestor/cargos/cargos.component.ts
@@ -1,15 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { LaravelService } from '../../laravel.service';
 
 @Component({
   selector: 'app-cargos',
   templateUrl: './cargos.component.html',
   styleUrls: ['./cargos.component.css']
 })
-export class CargosComponent implements OnInit {
 
-  constructor() { }
+export class CargosComponent {
+  public loading = true
 
-  ngOnInit() {
+  public cargos
+
+  public newCargo: object = {
+    nome: '',
+    duracao_meses: 0
   }
 
+  constructor(private lara: LaravelService) {
+    this.lara.all('cargos').then((e: any) => {
+      this.cargos = e.json()
+
+      this.loading = false
+    })
+  }
+  
+  post() {
+    console.log(this.newCargo)
+    /*this.lara.post(this.newCargo, 'cargo', '', res => {
+      alert('Salvo')
+      window.location.reload()
+    })*/
+  }
+
+  put(cargo, input, input2) {
+    cargo.nome = input
+    cargo.duracao_meses = input2
+
+    this.lara.post(cargo, 'cargoEdit/' + cargo.id, '', res => {
+      alert('Salvo')
+    })
+  }
+
+  delete(id) {
+    this.lara.post(null, 'cargoDelete/' + id, '', res => {
+      alert('Deletado')
+      window.location.reload()
+    })
+  }
 }
